74db3fb44317b7309c0b2f9f1f03118282e747e3
AnatoliyOcheretnyi <svito014@gmail.com>
2026-01-12 22:46:15 +0200

    fix(front): fixed issue with useSearchParams

--- apps/frontend/app/search/page.tsx
+++ apps/frontend/app/search/page.tsx
@@ -1,9 +1,12 @@
-"use client";
-
 export const dynamic = "force-dynamic";
 
+import { Suspense } from "react";
 import { SearchScreen } from "../../src/features/search/screens/SearchScreen";
 
 export default function SearchPage() {
-  return <SearchScreen />;
+  return (
+    <Suspense fallback={null}>
+      <SearchScreen />
+    </Suspense>
+  );
 }
